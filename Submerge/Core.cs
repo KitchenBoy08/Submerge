52831db013815456a7d9eb0e272b000f86153193
KitchenBoy <coleton.trenton@outlook.com>
2025-07-28 09:41:13 -0500

    Add C# Project

---
+++ Submerge/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(Submerge.Core), "Submerge", "1.0.0", "KitchenBoy", null)]
+[assembly: MelonGame("Unknown Worlds", "Subnautica")]
+
+namespace Submerge
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
